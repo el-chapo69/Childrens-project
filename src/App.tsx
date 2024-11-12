b9aeee5125d03566f7ebe944cf7055416ed22c13
BLACKxs123 <fonteevans123@gmail.com>
2024-11-12 15:03:42 +0300

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import DonatePage from './pages/DonatePage';
+import AboutPage from './pages/AboutPage';
+import SignupPage from './pages/SignupPage';
+import HomeDetailsPage from './pages/HomeDetailsPage';
+import AdminDashboard from './pages/AdminDashboard';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-white">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/donate" element={<DonatePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/home/:id" element={<HomeDetailsPage />} />
+          <Route path="/admin" element={<AdminDashboard />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
